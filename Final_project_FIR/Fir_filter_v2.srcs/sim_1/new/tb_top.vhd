b702535e8995cc9b82d8e46c155d60dc2892a608
Einlar <franceco.manzali@studenti.unipd.it>
2020-08-21 12:29:26 +0200

    + comments in code, + python simulation and plot

---
+++ Final_project_FIR/Fir_filter_v2.srcs/sim_1/new/tb_top.vhd
@@ -0,0 +1,152 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use STD.textio.all;
+
+
+entity tb_top is
+--  Port ( );
+end tb_top;
+
+architecture Behavioral of tb_top is
+constant ADDR_WIDTH : integer := 10;
+constant N_TAPS     : integer := 16; 
+constant DATA_WIDTH : integer := 32;
+component top is
+    Port ( 
+        clk     : in  std_logic;
+        rstb    : in  std_logic;
+        i_coeff : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+        addr_a  : in  std_logic_vector(ADDR_WIDTH - 1 downto 0);
+        we_a    : in  std_logic;
+        write_a : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+        read_a  : out std_logic_vector(DATA_WIDTH-1 downto 0);
+        n_state : out natural range 0 to 3;
+        load    : in  std_logic := '0'
+    );
+end component;
+    
+    signal clk        : std_logic := '0';
+    signal rstb, we_a : std_logic;
+    
+    signal i_coeff    : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal load       : std_logic := '0';
+    
+    signal write_a	  : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal read_a	  : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal n_state    : natural range 0 to 3;
+    signal addr_a     : std_logic_vector(ADDR_WIDTH - 1 downto 0);
+    
+    file file_VECTORS : text;
+    file file_RESULTS : text;
+    file file_COEFFICIENTS : text;
+
+begin  
+
+    DUT : top
+    port map (
+        clk     => clk,               
+        rstb    => rstb,             
+        i_coeff => i_coeff,             
+        addr_a  => addr_a,
+        we_a    => we_a,
+        write_a => write_a,             
+        read_a  => read_a,
+        n_state => n_state,
+        load    => load
+    );             
+
+  
+    pl : process --Clock generation
+    begin
+        clk <= '0';
+        wait for 5 ns;
+        clk <= '1';
+        wait for 5 ns;
+    end process; --pl
+  
+    WaveGen_Proc : process --Main process
+    variable CurrentLine    : line;
+    variable v_ILINE        : line;
+    variable v_TAPS         : line;
+    variable v_OLINE        : line;
+    variable i_data_integer : integer := 0;
+    variable o_data_integer : integer := 0;
+    variable i_coeff_integer: integer := 0;
+    
+    begin
+        file_open(file_VECTORS, "input_vectors.txt", read_mode);
+        file_open(file_RESULTS, "output_results.txt", write_mode);
+        file_open(file_COEFFICIENTS, "fir_taps.txt", read_mode);
+        
+        --Reset phase
+        we_a <= '0';
+        rstb <= '1';
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        rstb <= '0';
+        
+        --Load phase
+        addr_a <= (others => '0');
+        we_a <= '1'; 
+        wait until rising_edge(clk); --Propagate state
+        
+        --Loop over lower half of RAM and load samples from file
+        for i in 0 to 2 ** (ADDR_WIDTH - 1) - 1 loop 
+            if not endfile(file_VECTORS) then
+                readline(file_VECTORS, v_ILINE);
+                read(v_ILINE, i_data_integer);
+                write_a <= std_logic_vector(to_signed(i_data_integer, write_a'length));
+            else
+                write_a <= (others => '0'); --Pad with trailing 0s
+            end if;
+            
+            wait until rising_edge(clk);
+            
+            addr_a <= std_logic_vector(unsigned(addr_a) + 1);
+        end loop;
+        
+        --Read/write phase
+        we_a <= '0'; 
+        wait until rising_edge(clk); --Propagate state
+        
+        --Load coefficients
+        load <= '1'; 
+        for i in 0 to N_TAPS-1 loop
+            readline(file_COEFFICIENTS, v_TAPS);
+            read(v_TAPS, i_coeff_integer);
+            i_coeff <= std_logic_vector(to_signed(i_coeff_integer, i_coeff'length));
+            wait until rising_edge(clk); --Wait for loading
+            wait until rising_edge(clk);
+        end loop;
+        load <= '0';
+        
+        --Wait for computations (i.e. until the FPGA is idle)
+        while n_state /= 0 loop
+            wait until rising_edge(clk); 
+        end loop;
+        
+        --Output phase
+        --The first N_TAPS + 1 addresses contain gibberish (due to the time needed for initializing the FIR)
+        --so they are skipped
+        
+        addr_a <= std_logic_vector(to_unsigned(N_TAPS + 2 + 512, addr_a'length)); --Start from N_TAPS + 2
+        wait until rising_edge(clk); -- Propagate state
+        
+        for i in N_TAPS + 3 to 2 ** (ADDR_WIDTH - 1) - 1 loop --Loop over the upper half of RAM
+            addr_a <= std_logic_vector(to_unsigned(i + 512, addr_a'length));
+            wait until rising_edge(clk);
+            
+            o_data_integer := to_integer(signed(read_a)); --Write to file
+            write(v_OLINE, o_data_integer, left, DATA_WIDTH);
+            writeline(file_RESULTS, v_OLINE);  
+        
+        end loop;
+        
+        file_close(file_VECTORS);
+        file_close(file_RESULTS);
+        
+        wait;
+    end process WaveGen_Proc;
+        
+end Behavioral;
---
+++ Final_project_FIR/Fir_filter_v2.srcs/sources_1/new/fir_filter.vhd
@@ -0,0 +1,94 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity fir_filter_4 is
+    generic(
+        N_TAPS     : natural;
+        DATA_WIDTH : natural
+    );
+    port (
+        i_clk   : in  std_logic;
+        i_rstb  : in  std_logic;
+        we_fir  : in  std_logic;
+        load    : in  std_logic := '0';
+        i_coeff : in  std_logic_vector(DATA_WIDTH -1 downto 0);
+        i_data  : in  std_logic_vector(DATA_WIDTH -1 downto 0);
+        o_data  : out std_logic_vector(DATA_WIDTH -1 downto 0)
+    );
+end fir_filter_4;
+
+architecture rtl of fir_filter_4 is
+
+    type t_data_pipe is array (0 to N_TAPS-1) of signed(DATA_WIDTH -1 downto 0); 
+    type t_coeff is array (0 to N_TAPS-1) of signed(DATA_WIDTH -1 downto 0); 
+    type t_mult is array (0 to N_TAPS-1) of signed(2*DATA_WIDTH-1 downto 0); 
+    type t_add_st0 is array (0 to N_TAPS/2 -1) of signed(2*DATA_WIDTH downto 0); 
+    
+    signal r_coeff   : t_coeff; 
+    signal p_data    : t_data_pipe; 
+    signal r_mult    : t_mult; 
+    signal r_add_st0 : t_add_st0; 
+    signal r_add_st1 : signed(2*DATA_WIDTH+1 downto 0); 
+
+begin
+
+    p_input : process (i_rstb, i_clk)
+    begin
+        if(i_rstb = '1') then --Reset all signals
+            p_data  <= (others => (others => '0'));
+            r_coeff <= (others => (others => '0'));
+        elsif(rising_edge(i_clk) and we_fir = '1') then --Insert new sample at the beginning, shift the others
+            p_data <= signed(i_data)&p_data(0 to p_data'length-2);
+            if load = '1' then --Load also a coefficient
+                r_coeff <= signed(i_coeff)&r_coeff(0 to r_coeff'length-2);
+            end if;
+        end if;
+    end process p_input;
+
+    p_mult : process (i_rstb, i_clk) --Multiply the samples with the coefficients
+    begin
+        if(i_rstb = '1') then
+            r_mult <= (others => (others => '0'));
+        elsif(rising_edge(i_clk)) then 
+            for k in 0 to N_TAPS-1 loop
+                r_mult(k) <= p_data(k) * r_coeff(k);
+            end loop;
+        end if;
+    end process p_mult;
+
+    p_add_st0 : process (i_rstb, i_clk) --Sum reduction: sum successive pairs
+    begin
+        if(i_rstb = '1') then
+            r_add_st0 <= (others => (others => '0'));
+        elsif(rising_edge(i_clk)) then
+            for k in 0 to N_TAPS/2-1 loop
+                r_add_st0(k) <= resize(r_mult(2*k), 2*DATA_WIDTH+1) + resize(r_mult(2*k+1), 2*DATA_WIDTH+1);
+            end loop;
+        end if;
+    end process p_add_st0;
+
+    p_add_st1 : process (i_rstb, i_clk) --Sum reduction
+    variable tmp: signed(2*DATA_WIDTH+1 downto 0):= (others => '0');
+    begin
+        tmp := (others => '0');
+        if(i_rstb = '1') then
+            r_add_st1 <= (others => '0');
+        elsif(rising_edge(i_clk)) then 
+            for k in 0 to N_TAPS/2-1 loop
+                tmp := tmp + resize(r_add_st0(k), 2*DATA_WIDTH+2);
+            end loop;
+            r_add_st1 <= tmp;
+        end if;
+    end process p_add_st1;
+
+    p_output : process (i_rstb, i_clk) --Compute output
+    begin
+        if(i_rstb = '1') then
+            o_data <= (others => '0');
+        elsif(rising_edge(i_clk)) then 
+            o_data <= std_logic_vector(r_add_st1(DATA_WIDTH-1 downto 0)); 
+        end if;
+    end process p_output;
+    
+end rtl;
